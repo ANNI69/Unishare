4f154457d9f2fb98ec1ff0a912f415de78ed43b0
Aniruddh <codebyanii@gmail.com>
2024-08-04 04:01:05 -0700

    Popular Comm

--- frontend/src/components/PopularCommunities.tsx
+++ frontend/src/components/PopularCommunities.tsx
@@ -1,75 +1,55 @@
-
 const PopularCommunities = () => {
+  const popularCommunities = [
+    {
+      name: "DmceMemers",
+      description: "Meme Community of DMCE",
+      image:
+        "https://i.pravatar.cc/150?img=1",
+    },
+    {
+      name: "AthrvaCompsDept",
+      description: "Comps Department of ACE",
+      image:
+        "https://i.pravatar.cc/150?img=2",
+    },
+    {
+      name: "VjtiCompsDept",
+      description: "Comps Department of VJTI",
+      image:
+        "https://i.pravatar.cc/150?img=3",
+    },
+  ];
   return (
     <>
       <div className="flex flex-col h-full max-w-[400px] p-2">
-      <div className="flex w-full flex-col px-3 overflow-hidden bg-background md:shadow-xl rounded-lg shadow-lg">
-        <h4 className="text-xl font-bold">Popular Communities</h4>
-        <a href="/communities" className="text-sm font-semibold text-gray-90">
-          View all
-        </a>
-        <div className="flex flex-col mt-2">
-          <div className="flex items-center justify-between py-2">
-            <div className="flex items-center">
-              <img
-                src="https://www.redditstatic.com/avatars/avatar_default_01_24A0ED.png"
-                alt=""
-                className="h-8 w-8 rounded-full"
-              />
-              <div className="flex flex-col ml-2">
-                <p className="text-sm font-semibold">r/AskReddit</p>
-                <p className="text-xs font-light">Ask Reddit...</p>
-              </div>
-            </div>
-            <button className="text-xs font-semibold text-blue-400">Join</button>
-          </div>
-          <div className="flex items-center justify-between py-2">
-            <div className="flex items-center">
-              <img
-                src="https://www.redditstatic.com/avatars/avatar_default_02_24A0ED.png"
-                alt=""
-                className="h-8 w-8 rounded-full"
-              />
-              <div className="flex flex-col ml-2">
-                <p className="text-sm font-semibold">r/AskReddit</p>
-                <p className="text-xs font-light">Ask Reddit...</p>
-              </div>
-            </div>
-            <button className="text-xs font-semibold text-blue-400">Join</button>
-          </div>
-          <div className="flex items-center justify-between py-2">
-            <div className="flex items-center">
-              <img
-                src="https://www.redditstatic.com/avatars/avatar_default_03_24A0ED.png"
-                alt=""
-                className="h-8 w-8 rounded-full"
-              />
-              <div className="flex flex-col ml-2">
-                <p className="text-sm font-semibold">r/AskReddit</p>
-                <p className="text-xs font-light">Ask Reddit...</p>
-              </div>
-            </div>
-            <button className="text-xs font-semibold text-blue-400">Join</button>
-          </div>
-          <div className="flex items-center justify-between py-2">
-            <div className="flex items-center">
-              <img
-                src="https://www.redditstatic.com/avatars/avatar_default_04_24A0ED.png"
-                alt=""
-                className="h-8 w-8 rounded-full"
-              />
-              <div className="flex flex-col ml-2">
-                <p className="text-sm font-semibold">r/AskReddit</p>
-                <p className="text-xs font-light">Ask Reddit...</p>
-              </div>
-            </div>
-            <button className="text-xs font-semibold text-blue-400">Join</button>
+        <div className="flex w-full flex-col px-3 overflow-hidden bg-background md:shadow-xl rounded-lg shadow-lg">
+          <h4 className="text-xl font-bold">Popular Communities</h4>
+          <div className="flex flex-col mt-2">
+            {
+              popularCommunities.map((community) => (
+                <div className="flex items-center justify-between p-2 my-2 bg-background rounded-lg">
+                  <div className="flex items-center">
+                    <img
+                      src={community.image}
+                      alt="community"
+                      className="w-8 h-8 rounded-full"
+                    />
+                    <div className="flex flex-col ml-2">
+                      <h4 className="text-sm font-bold">{community.name}</h4>
+                      <p className="text-xs">{community.description}</p>
+                    </div>
+                  </div>
+                  <button className="px-2 py-1 text-xs text-white bg-primary-alt rounded-lg">
+                    Join
+                  </button>
+                </div>
+              ))
+            }
           </div>
         </div>
       </div>
-      </div>
     </>
-  )
-}
+  );
+};
 
-export default PopularCommunities
+export default PopularCommunities;
